0c0944b721101badf56c7ea341d387ff229c6eba
Jamie Flynt <jamflynt@gmail.com>
2021-11-05 23:57:19 -0400

    Created if statements in eventListeners. Created update function for eventListners to update CSS styling and show progress. Initial version finished.

--- script.js
+++ script.js
@@ -12,8 +12,8 @@ next.addEventListener(`click`, () => {
     if (currentActive > circle.length) {
         currentActive = circle.length;
     }
-
-    console.log(currentActive);
+    
+    update();
   });
 
 prev.addEventListener(`click`, () => {
@@ -24,5 +24,25 @@ prev.addEventListener(`click`, () => {
         currentActive = 1;
     }
 
-    console.log(currentActive);
+    update();
   });
+
+  function update() {
+      circle.forEach((circle, index) => {
+          if(index < currentActive){
+              circle.classList.add(`active`)
+          } else {
+              circle.classList.remove(`active`)
+          }
+      });
+      const actives = document.querySelectorAll(`.active`);
+      progress.style.width = (actives.length - 1) / (circle.length - 1)*100+`%`; // '%' adds unit for CSS styling
+      if (currentActive === 1) {
+          prev.disabled = true;
+      } else if(currentActive === circle.length){
+          next.disabled = true;
+      } else {
+          prev.disabled = false;
+          next.disabled = false;
+      }
+  };
