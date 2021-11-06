f6b3141fb4f6858b07e63e2c5af186ed955b8064
Jamie Flynt <jamflynt@gmail.com>
2021-11-05 23:41:44 -0400

    Added event listeners to buttons.

--- script.js
+++ script.js
@@ -8,5 +8,21 @@ let currentActive = 1
 next.addEventListener(`click`, () => {
     currentActive ++;
 
+    // For keeping line within the number of circles in progress line
+    if (currentActive > circle.length) {
+        currentActive = circle.length;
+    }
+
     console.log(currentActive);
-})
+  });
+
+prev.addEventListener(`click`, () => {
+    currentActive --;
+
+    // For keeping line within the number of circles in progress line
+    if (currentActive < 1) {
+        currentActive = 1;
+    }
+
+    console.log(currentActive);
+  });
